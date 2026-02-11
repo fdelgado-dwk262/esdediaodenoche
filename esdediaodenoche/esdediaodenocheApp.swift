5c1217fa11db0d0a2f6963a0c21e3e2a0428db14
Fernando <dwk262@gmail.com>
2026-02-11 09:45:59 +0100

    Initial Commit

---
+++ esdediaodenoche/esdediaodenocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  esdediaodenocheApp.swift
+//  esdediaodenoche
+//
+//  Created by Equipo 9 on 11/2/26.
+//
+
+import SwiftUI
+
+@main
+struct esdediaodenocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
